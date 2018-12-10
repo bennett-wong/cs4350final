c67f87e34d258620c50edc67b58602574f9a068f
bennett-wong <bennettryuma@hotmail.com>
2018-12-10 11:57:37 -0800

    Creating methods to cleanup code

--- 4350Final/src/jdbc/Driver.java
+++ 4350Final/src/jdbc/Driver.java
@@ -1,4 +1,3 @@
-package jdbc;
 import java.sql.*;
 import java.util.Scanner;
 public class Driver {
@@ -17,23 +16,7 @@ public class Driver {
 				 String StartLocationName = s1.next();
 				 String DestinationName = s1.next();
 				 String Date = s1.next();
-				 try {
-						
-						Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3380/data", user, password);
-						
-						Statement myStatement = myConn.createStatement();
-						
-						ResultSet myRs = myStatement.executeQuery("select TOF.ScheduledStartTime, TOF.ScheduledArrivalTime, "
-								+ "TOF.DriverName, TOF.BusID FROM TripOffering TOF, Trip T WHERE TOF.TripNumber = T.TripNumber AND T.StartLocationName = " + 
-								StartLocationName + " AND T.DestinationName = " + DestinationName + " AND TOF.Date = " + Date);
-						while(myRs.next()) {
-							System.out.print(myRs.getString("ScheduledStartTime") + ", " + myRs.getString("ScheduledArrivalTime") + ", " 
-							+ myRs.getString("DriverName") + ", " + myRs.getString("BusID"));
-							}
-					}
-					catch(Exception exc) {
-						exc.printStackTrace();
-					}
+				 displayScheduledTrips(StartLocationName, DestinationName, Date);
 			 	break;
 			 case 2: 
 				 System.out.println("Please enter your option");
@@ -41,12 +24,15 @@ public class Driver {
 				 int choice1 = s1.nextInt();
 				 switch(choice1) {
 				 case 1:
-					 System.out.println("Enter in TripNumber");
+					 System.out.print("Enter in TripNumber: ");
 					 int TripNumber1 = s1.nextInt();
-					 System.out.println("Enter in Date");
+					 System.out.print("\n");
+					 System.out.print("Enter in Date: ");
 					 String Date1 = s1.next();
-					 System.out.println("Enter in Schedule Start Time");
+					 System.out.print("\n");
+					 System.out.print("Enter in Schedule Start Time: ");
 					 String ScheduledStartTime1 = s1.next();
+					 System.out.print("\n");
 					 try {
 							
 							Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3380/data", user, password);
@@ -143,46 +129,17 @@ public class Driver {
 			 case 3:
 				 System.out.println("Enter in TripNumber");
 				 int TripNumber = s1.nextInt();
-				 try {
-						
-						Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3380/data", user, password);
-						
-						Statement myStatement = myConn.createStatement();
-						
-						ResultSet myRs = myStatement.executeQuery("SELECT TS.TripNumber, TS.StopNumber, TS.SequenceNumber, TS.DrivingTime FROM TripStopInfo TS "
-								+ "WHERE TS.TripNumber = " + TripNumber);
-						while(myRs.next()) {
-							System.out.print(myRs.getString("TripNumber") + ", " + myRs.getString("StopNumber") + ", " 
-							+ myRs.getString("SequenceNumber") + ", " + myRs.getString("DrivingTime"));
-							}
-					}
-					catch(Exception exc) {
-						exc.printStackTrace();
-					}
+				 displayTripStops(TripNumber);
              	break;
 			 case 4:  
 				 System.out.println("Please enter in driver and date for weekly schedule");
 				 System.out.print("Driver: ");
 				 String DriverName4 = s1.next();
+				 System.out.print("\n");
 				 System.out.print("Date: ");
 				 String Date4 = s1.next();
-				 try {
-						
-						Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3380/data", user, password);
-						
-						Statement myStatement = myConn.createStatement();
-						
-						ResultSet myRs = myStatement.executeQuery("SELECT T.TripNumber, T.StartLocationName, T.DestinationName, "
-								+ "TOF.ScheduledStartTime, TOF.ScheduledArrivalTime FROM Trip T, TripOffering TOF WHERE T.TripNumber = TOF.TripNumber AND TOF.DriverName = " 
-								+ DriverName4 + " AND TOF.Date = " + Date4);
-						while(myRs.next()) {
-							System.out.print(myRs.getString("TripNumber") + ", " + myRs.getString("StartLocationName") + ", " 
-							+ myRs.getString("DestinationName") + ", " + myRs.getString("ScheduledStartTime") + ", " + myRs.getString("ScheduledArrivalTime"));
-							}
-					}
-					catch(Exception exc) {
-						exc.printStackTrace();
-					}
+				 System.out.print("\n");
+				 displaySchedule(DriverName4, Date4);
 				 break;
 			 case 5:
 				 System.out.println("Please enter in the driver name and Driver Telephone Number");
@@ -190,23 +147,27 @@ public class Driver {
 				 String DriverName5 = s1.next();
 				 System.out.print("\n");
 				 System.out.print("Driver's Telephone Number: ");
-				 String DriverTelephoneNumber5 = s1.nextInt();
+				 int DriverTelephoneNumber5 = s1.nextInt();
 				 addDriver(DriverName5, DriverTelephoneNumber5);
              	break;
 			 case 6:
 				 System.out.println("Please enter in the BusID, Model, and Year");
 				 System.out.print("BusID: ");
-				 String BusID6 = s1.nextInt();
+				 int BusID6 = s1.nextInt();
 				 System.out.print("\n");
 				 System.out.print("Model: ");
 				 String Model = s1.next();
 				 System.out.print("\n");
 				 System.out.print("Year: ");
-				 String Year = s1.nextInt();
+				 int Year = s1.nextInt();
 				 System.out.print("\n");
-				 addBus(BusID6, Model, Year)
+				 addBus(BusID6, Model, Year);
 			 	break;
-			 case 7:  
+			 case 7:
+				 System.out.print("Please enter in BusID to delete: ");
+				 int BusID7 = s1.nextInt();
+				 System.out.print("\n");
+				 deleteBus(BusID7);
 			 	break;
 			 case 8:  
 			 	break;
@@ -219,6 +180,7 @@ public class Driver {
 		}
 		
 	}
+	
 	public static void menu() {
 		System.out.println("1. Display schedule of trips");
 		System.out.println("2. Edit schedule");
@@ -230,38 +192,154 @@ public class Driver {
 		System.out.println("8. Record actual data of a given trip");
 		System.out.println("9. Exit");
 	}
+	
 	public static void menu2() {
 		System.out.println("1. Delete a trip offering specified by Trip#, Date, and ScheduledStartTime");
 		System.out.println("2. Add a set of trip offerings assuming the values of all attributes are given");
 		System.out.println("3. Change the driver for a given Trip offering (i.e given TripNumber, Date, ScheduledStartTime)");
 		System.out.println("4. Change the bus for a given Trip offering.");
 	}
-	public static void addDriver(String DriverName, String DriverTelephoneNumber) {
+	
+	/*
+	 * #1
+	 */
+	
+	public static void displayScheduledTrips(String StartLocationName, String DestinationName, String Date) {
 		try {
-			
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3380/data", user, password);
+			//Edit to fit your database
+			Connection myConn = DriverManager.getConnection(url, user, password);
 			
 			Statement myStatement = myConn.createStatement();
 			
-			String sql = "INSERT INTO driver (DriverName, DriverTelephoneNumber) VALUES (" + DriverName + ", " + DriverTelephoneNumber + ")";
-			myStatement.exectuteUpdate(sq1);
+			ResultSet myRs = myStatement.executeQuery("select TOF.ScheduledStartTime, TOF.ScheduledArrivalTime, "
+					+ "TOF.DriverName, TOF.BusID FROM TripOffering TOF, Trip T WHERE TOF.TripNumber = T.TripNumber AND T.StartLocationName = " + 
+					StartLocationName + " AND T.DestinationName = " + DestinationName + " AND TOF.Date = " + Date);
+			while(myRs.next()) {
+				System.out.print(myRs.getString("ScheduledStartTime") + ", " + myRs.getString("ScheduledArrivalTime") + ", " 
+				+ myRs.getString("DriverName") + ", " + myRs.getString("BusID"));
+				}
 		}
 		catch(Exception exc) {
 			exc.printStackTrace();
 		}
 	}
-	public static void addBus(int BusID, String Model, int Year) {
+	
+
+	//======================================================================//
+	
+	
+	
+	
+	/*
+	 * #3
+	 */
+	
+	public static void displayTripStops(int TripNumber) {
 		try {
+			//Edit to fit your database
+			Connection myConn = DriverManager.getConnection(url, user, password);
 			
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3380/data", user, password);
+			Statement myStatement = myConn.createStatement();
 			
-			Statement myStatement = .myConn.createStatement();
-			
-			String sql = "INSERT INTO bus (BusID, Model, Year) VALUES (" + BusID + ", " + Model + ", " + Year + ")";
-			myStatement.exectuteUpdate(sq1);		
+			ResultSet myRs = myStatement.executeQuery("SELECT TS.TripNumber, TS.StopNumber, TS.SequenceNumber, TS.DrivingTime FROM TripStopInfo TS "
+					+ "WHERE TS.TripNumber = " + TripNumber);
+			while(myRs.next()) {
+				System.out.print(myRs.getString("TripNumber") + ", " + myRs.getString("StopNumber") + ", " 
+				+ myRs.getString("SequenceNumber") + ", " + myRs.getString("DrivingTime"));
+				}
 		}
 		catch(Exception exc) {
 			exc.printStackTrace();
 		}
+	}
+	
+	/*
+	 * #4
+	 */
+	
+	public static void displaySchedule(String DriverName, String Date) {
+		try {
+			//Edit to fit your database
+			Connection myConn = DriverManager.getConnection(url, user, password);
+			
+			Statement myStatement = myConn.createStatement();
+			
+			ResultSet myRs = myStatement.executeQuery("SELECT T.TripNumber, T.StartLocationName, T.DestinationName, "
+					+ "TOF.ScheduledStartTime, TOF.ScheduledArrivalTime FROM Trip T, TripOffering TOF WHERE T.TripNumber = TOF.TripNumber AND TOF.DriverName = " 
+					+ DriverName + " AND TOF.Date = " + Date);
+			while(myRs.next()) {
+				System.out.print(myRs.getString("TripNumber") + ", " + myRs.getString("StartLocationName") + ", " 
+				+ myRs.getString("DestinationName") + ", " + myRs.getString("ScheduledStartTime") + ", " + myRs.getString("ScheduledArrivalTime"));
+				}
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	
+	/*
+	 * #5
+	 */
+	
+	public static void addDriver(String DriverName, int DriverTelephoneNumber) {
+		try {
+			//Edit to fit your database
+			Connection myConn = DriverManager.getConnection(url, user, password);
+			
+			Statement myStatement = myConn.createStatement();
+			
+			String sql = "INSERT INTO driver (DriverName, DriverTelephoneNumber) VALUES (" + DriverName + ", " + DriverTelephoneNumber + ")";
+			myStatement.executeUpdate(sql);
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	
+	/*
+	 * #6
+	 */
+	
+	public static void addBus(int BusID, String Model, int Year) {
+		try {
+			//Edit to fit your database
+			Connection myConn = DriverManager.getConnection(url, user, password);
+			
+			Statement myStatement = myConn.createStatement();
+			
+			String sql = "INSERT INTO bus (BusID, Model, Year) VALUES (" + BusID + ", " + Model + ", " + Year + ")";
+			myStatement.executeUpdate(sql);		
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	
+	/*
+	 * #7
+	 */
+	
+	public static void deleteBus(int BusID) {
+		try {
+			//Edit to fit your database
+			Connection myConn = DriverManager.getConnection(url, user, password);
+			
+			Statement myStatement = myConn.createStatement();
+			
+			String sql = "DELETE FROM bus WHERE BusID = " + BusID;
+			myStatement.executeUpdate(sql);		
+			System.out.println("Bus with ID " + BusID + " has been deleted.");
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	
+	/*
+	 * #8
+	 */
+	
+	public static void insertData() {
+		
 	}
 }	
